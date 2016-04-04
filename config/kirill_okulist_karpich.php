a8a197da9756bab14dc1db188c507674659d70d7
Yuri Neudachin <yurineydachin@mail.ru>
2016-04-04 16:41:32 +0300

    Add configs for Kirill today

---
+++ config/kirill_okulist_karpich.php
@@ -0,0 +1,22 @@
+<?php
+//3648|Карпыч|Виктория|Ростиславовна|||Офтальмологи|Офтальмология детской поликлиники|0901052|1085|3648
+define('TIME_FROM',       '2016-04-02 20:55:00',true);
+define('TIME_TO',         '2016-04-02 20:10:00',true);
+define('EMAIL',           'yurineydachin@mail.ru',true);
+define('POLIS',           '141712440',true);
+define('BIRTHDAY',        '01.03.2016',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '3648',true);
+define('DATE_APPINTMENT', '2016-04-18',true);
+define('SCENERY',         1,true);
+//define('DEBUG',           1,true);
+
+$timePriority = array(
+    '09:00' => 1,
+    '10:00' => 3,
+    '14:00' => 4,
+    '15:00' => 6,
+    '17:00' => 5,
+    '18:19' => 6,
+    '23:59' => 2,
+);
