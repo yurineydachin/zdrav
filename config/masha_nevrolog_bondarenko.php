9389ed7e0ea139fad49c3d92838b0daa288619ce
Yuri Neudachin <yurineydachin@mail.ru>
2016-04-25 07:00:54 +0300

    add Masha configs and logs

---
+++ config/masha_nevrolog_bondarenko.php
@@ -0,0 +1,18 @@
+<?php
+//1323|Бондаренко|Ираида|Лукъяновна|||Неврологи|Неврология детской поликлиники|0901052|1063|1323
+define('TIME_FROM',       '2016-04-12 20:58:00',true);
+define('TIME_TO',         '2016-04-12 21:02:00',true);
+define('EMAIL',           'yurineydachin@mail.ru',true);
+define('POLIS',           '7193689784000012',true);
+define('BIRTHDAY',        '15.06.2013',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '1323',true);
+define('DATE_APPINTMENT', '2016-04-26',true);
+define('SCENERY',         1,true);
+define('DEBUG',           1,true);
+
+$timePriority = array(
+    '10:00' => 2,
+    '17:00' => 1,
+    '23:59' => 2,
+);
---
+++ config/masha_okulist_karpich.php
@@ -0,0 +1,18 @@
+<?php
+//3648|Карпыч|Виктория|Ростиславовна|||Офтальмологи|Офтальмология детской поликлиники|0901052|1085|3648
+define('TIME_FROM',       '2016-04-12 20:58:00',true);
+define('TIME_TO',         '2016-04-12 21:02:00',true);
+define('EMAIL',           'yurineydachin@mail.ru',true);
+define('POLIS',           '7193689784000012',true);
+define('BIRTHDAY',        '15.06.2013',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '3648',true);
+define('DATE_APPINTMENT', '2016-04-26',true);
+define('SCENERY',         1,true);
+define('DEBUG',           1,true);
+
+$timePriority = array(
+    '10:00' => 2,
+    '17:00' => 1,
+    '23:59' => 2,
+);
