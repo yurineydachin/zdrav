77972d5334a9fae5e37f16939f1058260399697b
Yuri Neudachin <yurineydachin@mail.ru>
2016-05-19 14:59:28 +0300

    add Kirill and Kristina 2016-06-02

--- config/kirill_nevrolog_bondarenko.php
+++ config/kirill_nevrolog_bondarenko.php
@@ -1,14 +1,14 @@
 <?php
 //1323|Бондаренко|Ираида|Лукъяновна|||Неврологи|Неврология детской поликлиники|0901052|1063|1323
 //define('DEBUG',           1,true);
-define('TIME_FROM',       '2016-05-18 20:58:00',true);
-define('TIME_TO',         '2016-05-18 21:02:00',true);
+define('TIME_FROM',       '2016-05-19 20:58:00',true);
+define('TIME_TO',         '2016-05-19 21:02:00',true);
 define('EMAIL',           'yurineydachin@mail.ru',true);
 define('POLIS',           '5096389748000313',true);
 define('BIRTHDAY',        '01.03.2016',true);
 define('LPU_CODE',        '0901052',true);
 define('DOCTOR',          '1323',true);
-define('DATE_APPINTMENT', '2016-06-01',true);
+define('DATE_APPINTMENT', '2016-06-02',true);
 
 $timePriority = array(
     '09:00' => 1,
--- config/kirill_ortoped_rikova.php
+++ config/kirill_ortoped_rikova.php
@@ -1,13 +1,13 @@
 <?php
 //1325|Рыкова|Инна|Юрьевна|||Травматологи-ортопеды|Ортопедия детской поликлиники|0901052|1163|1325
-define('TIME_FROM',       '2016-05-18 20:58:00',true);
-define('TIME_TO',         '2016-05-18 21:02:00',true);
-define('EMAIL',           'yurineydachin@mail.ru',true);
+define('TIME_FROM',       '2016-05-19 20:58:00',true);
+define('TIME_TO',         '2016-05-19 21:02:00',true);
+define('EMAIL',           'yurineyda9hin@mail.ru',true);
 define('POLIS',           '5096389748000313',true);
 define('BIRTHDAY',        '01.03.2016',true);
 define('LPU_CODE',        '0901052',true);
 define('DOCTOR',          '1325',true);
-define('DATE_APPINTMENT', '2016-06-01',true);
+define('DATE_APPINTMENT', '2016-06-02',true);
 define('SCENERY',         1,true);
 //define('DEBUG',           1,true);
 
@@ -17,6 +17,6 @@ $timePriority = array(
     '14:00' => 4,
     '15:00' => 6,
     '17:00' => 5,
-    //'18:19' => 6,
-    //'23:59' => 2,
+    '18:19' => 6,
+    '23:59' => 2,
 );
