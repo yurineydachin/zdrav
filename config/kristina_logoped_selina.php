40f9be8957f27ffda35ca7271a270e14c485232d
Yuri Neudachin <yurineydachin@mail.ru>
2016-05-19 14:59:39 +0300

    add Kirill and Kristina 2016-06-02

---
+++ config/kristina_logoped_selina.php
@@ -0,0 +1,22 @@
+<?php
+//1346|Селина|Елена|Николаевна|||Педиатры|Логопед детской поликлиники|0901052|1159|1346
+define('TIME_FROM',       '2016-05-19 20:58:00',true);
+define('TIME_TO',         '2016-05-19 21:02:00',true);
+define('EMAIL',           'yurineyda9hin@mail.ru',true);
+define('POLIS',           '7754310890001528',true);
+define('BIRTHDAY',        '03.05.2013',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '1346',true);
+define('DATE_APPINTMENT', '2016-06-02',true);
+define('SCENERY',         1,true);
+//define('DEBUG',           1,true);
+
+$timePriority = array(
+    '09:00' => 1,
+    '10:00' => 3,
+    '14:00' => 4,
+    '15:00' => 6,
+    '17:00' => 5,
+    '18:19' => 6,
+    '23:59' => 2,
+);
