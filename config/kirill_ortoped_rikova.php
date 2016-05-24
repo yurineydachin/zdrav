d6b6750c39fb3a3f5b49b96cbb35a36416eb6511
Yury Neudachin <yneudachin@dev2.mskdc.lzd.co>
2016-05-24 09:34:17 +0300

    fourth try made appintment to ortoped for kirill

--- config/kirill_ortoped_rikova.php
+++ config/kirill_ortoped_rikova.php
@@ -1,13 +1,13 @@
 <?php
 //1325|Рыкова|Инна|Юрьевна|||Травматологи-ортопеды|Ортопедия детской поликлиники|0901052|1163|1325
-define('TIME_FROM',       '2016-05-23 20:58:00',true);
-define('TIME_TO',         '2016-05-23 21:02:00',true);
+define('TIME_FROM',       '2016-05-24 20:58:00',true);
+define('TIME_TO',         '2016-05-24 21:02:00',true);
 define('EMAIL',           'yurineyda9hin@mail.ru',true);
 define('POLIS',           '5096389748000313',true);
 define('BIRTHDAY',        '01.03.2016',true);
 define('LPU_CODE',        '0901052',true);
 define('DOCTOR',          '1325',true);
-define('DATE_APPINTMENT', '2016-06-06',true);
+define('DATE_APPINTMENT', '2016-06-07',true);
 define('SCENERY',         1,true);
 //define('DEBUG',           1,true);
 
