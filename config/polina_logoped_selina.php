4aa81fdc491234f914c6a22faad819f91dd9a740
Yury Neudachin <yneudachin@dev2.mskdc.lzd.co>
2016-05-26 23:36:00 +0300

    add ponina appointment

---
+++ config/polina_logoped_selina.php
@@ -0,0 +1,22 @@
+<?php
+//1346|Селина|Елена|Николаевна|||Педиатры|Логопед детской поликлиники|0901052|1159|1346
+define('TIME_FROM',       '2016-05-26 20:58:00',true);
+define('TIME_TO',         '2016-05-26 21:02:00',true);
+define('EMAIL',           'yurineyda9hin@mail.ru',true);
+define('POLIS',           '5091689772000310',true);
+define('BIRTHDAY',        '27.08.2013',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '1346',true);
+define('DATE_APPINTMENT', '2016-06-09',true);
+define('SCENERY',         1,true);
+//define('DEBUG',           1,true);
+
+$timePriority = array(
+    '09:00' => 1,
+    '10:00' => 3,
+    '14:00' => 4,
+    '15:00' => 6,
+    '17:00' => 5,
+    '18:19' => 6,
+    '23:59' => 2,
+);
