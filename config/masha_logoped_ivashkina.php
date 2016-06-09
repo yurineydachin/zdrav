8ae90127b971d15877beccbe91db9bd9a031ad15
Yury Neudachin <yneudachin@dev2.mskdc.lzd.co>
2016-06-09 16:07:44 +0300

    add Masha appintment

---
+++ config/masha_logoped_ivashkina.php
@@ -0,0 +1,22 @@
+<?php
+//2469|Ивашкина|Татьяна|Александровна|||Педиатры|Логопед детской поликлиники|0901052|1159|2469
+define('TIME_FROM',       '2016-06-08 20:58:00',true);
+define('TIME_TO',         '2016-06-08 21:02:00',true);
+define('EMAIL',           'yurineyda9hin@mail.ru',true);
+define('POLIS',           '7193689784000012',true);
+define('BIRTHDAY',        '15.06.2013',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '2469',true);
+define('DATE_APPINTMENT', '2016-06-22',true);
+define('SCENERY',         1,true);
+//define('DEBUG',           1,true);
+
+$timePriority = array(
+    '09:00' => 1,
+    '10:00' => 3,
+    '14:00' => 4,
+    '15:00' => 6,
+    '17:00' => 5,
+    '18:19' => 6,
+    '23:59' => 2,
+);
