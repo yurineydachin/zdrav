970e7a0a976093dfb7802243f1982917b3687c7f
Yuri Neudachin <yurineydachin@mail.ru>
2016-06-10 15:45:43 +0300

    add Dima appintment

---
+++ config/dima_oftalmolog_pargina.php
@@ -0,0 +1,22 @@
+<?php
+//3591|Паргина|Наталья|Михайловна|||Офтальмологи|Офтальмология детской поликлиники|0901052|1085|3591
+define('TIME_FROM',       '2016-06-10 20:58:00',true);
+define('TIME_TO',         '2016-06-10 21:02:00',true);
+define('EMAIL',           'yurineyda9hin@mail.ru',true);
+define('POLIS',           '5088889748000312',true);
+define('BIRTHDAY',        '01.11.2011',true);
+define('LPU_CODE',        '0901052',true);
+define('DOCTOR',          '3591',true);
+define('DATE_APPINTMENT', '2016-06-24',true);
+define('SCENERY',         1,true);
+//define('DEBUG',           1,true);
+
+$timePriority = array(
+    '09:00' => 1,
+    '10:00' => 3,
+    '14:00' => 4,
+    '15:00' => 6,
+    '17:00' => 5,
+    '18:19' => 6,
+    '23:59' => 2,
+);
